afa66d2746bcd9ea2208bb4788ffbd6b2ac02c6e
essejose <ze_pk2@hotmail.com>
2016-08-09 23:38:56 -0300

    resolvendo click

--- _js/game.js
+++ _js/game.js
@@ -34,7 +34,22 @@ var BubbleShoot = window.BubbleShoot || {};
 	};
 
 	
-
+	
+	var clickGameScreen = function(e){
+		var angle = BubbleShoot.ui.getBubbleAngle(curBubble.getSprite(),e);
+		var duration = 750;
+		var distance = 1000;
+		var distX = Math.sin(angle) * distance;
+		var distY = Math.cos(angle) * distance;
+		var bubbleCoords = BubbleShoot.ui.getBubbleCoords(curBubble.
+		getSprite());
+		var coords = {
+		x : bubbleCoords.left + distX,
+		y : bubbleCoords.top - distY
+		};
+	 	//BubbleShoot.ui.fireBubble(curBubble,coords,duration);
+ 
+	};
 
   return Game;
 
